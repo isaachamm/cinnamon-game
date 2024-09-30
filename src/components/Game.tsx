3fc921fbb75a0475430b09a4dc406cdde3d9e34d
Isaac Hamm <isaachamm3@gmail.com>
2024-09-30 16:17:27 -0600

    feat(cinnamon-game) added ability to change date and error-handling for dates outside of current dictionary range.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -18,10 +18,13 @@ export default function Game() {
 	const [guessedSynonyms, setGuessedSynonyms] = useState<Array<string>>([]);
 	const [guessMessage, setGuessMessage] = useState<string>('');
 
+	const [currDate, setCurrDate] = useState<Date>(new Date());
+	const [validDate, setValidDate] = useState<boolean>(true);
+
 
 	useEffect(() => {
 		fetchWord();
-	}, []);
+	}, [currDate]);
 
 	const fetchWord = async () => {
 
@@ -29,33 +32,45 @@ export default function Game() {
 			const dictionaryUrl = new URL('../assets/dictionary.json', import.meta.url).href;
 			const response = await fetch(dictionaryUrl);
 			const data = await response.json();
-			const currDate = new Date();
 			const currYear = currDate.getFullYear();
 			const currMonth = currDate.getMonth() + 1;
 			const currDay = currDate.getDate();
-			for (let i = 0; i < data.length; i++) {
-				const dateSplit = data[i].date.split('-')
-				const year = parseInt(dateSplit[0]);
-				const month = parseInt(dateSplit[1]);
-				const day = parseInt(dateSplit[2]);
-				if (year === currYear &&
-					month === currMonth &&
-					day === currDay
-				) {
+			const earliestDate = new Date(2024, 8, 9);
+			const latestDate = new Date();
+			if (
+				currDate > earliestDate &&
+				currDate < latestDate
+			) {
 
-					// Fix capitalization
-					const wordRaw: string = data[i].word;
-					const word = wordRaw.charAt(0).toUpperCase() + wordRaw.slice(1);
-					const partOfSpeechRaw: string = data[i].part_of_speech;
-					const partOfSpeech = partOfSpeechRaw.charAt(0).toUpperCase() + partOfSpeechRaw.slice(1);
+				setValidDate(true);
+
+				for (let i = 0; i < data.length; i++) {
+					const dateSplit = data[i].date.split('-')
+					const year = parseInt(dateSplit[0]);
+					const month = parseInt(dateSplit[1]);
+					const day = parseInt(dateSplit[2]);
+					if (
+						year === currYear &&
+						month === currMonth &&
+						day === currDay
+					) {
+
+						// Fix capitalization
+						const wordRaw: string = data[i].word;
+						const word = wordRaw.charAt(0).toUpperCase() + wordRaw.slice(1);
+						const partOfSpeechRaw: string = data[i].part_of_speech;
+						const partOfSpeech = partOfSpeechRaw.charAt(0).toUpperCase() + partOfSpeechRaw.slice(1);
 
 
-					setCorrectDefinitionWord(word);
-					setDefinitions(data[i].definitions);
-					setPartOfSpeech(partOfSpeech);
-					setSynonyms(data[i].synonyms);
-					return;
+						setCorrectDefinitionWord(word);
+						setDefinitions(data[i].definitions);
+						setPartOfSpeech(partOfSpeech);
+						setSynonyms(data[i].synonyms);
+						return;
+					}
 				}
+			} else {
+				setValidDate(false);
 			}
 			setCorrectDefinitionWord(defaultWord.word);
 			setDefinitions(defaultWord.definitions);
@@ -68,6 +83,41 @@ export default function Game() {
 			setPartOfSpeech(defaultWord.part_of_speech);
 			setSynonyms(defaultWord.synonyms);
 		}
+	}
+
+	const nextDate = () => {
+		var newDate = new Date(currDate.setDate(currDate.getDate() + 1));
+		if (newDate > new Date()) {
+			newDate = new Date(currDate.setDate(currDate.getDate() - 1));
+			setValidDate(false);
+			return;
+		}
+		setCurrDate(newDate);
+	}
+
+	const prevDate = () => {
+		var newDate = new Date(currDate.setDate(currDate.getDate() - 1));
+		if (newDate < new Date(2024, 8, 9)) {
+			newDate = new Date(currDate.setDate(currDate.getDate() + 1));
+			setValidDate(false);
+			return;
+		}
+		setCurrDate(newDate);
+	}
+
+	const onDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newDate = new Date(event.currentTarget.value);
+
+		if (newDate < new Date(2024, 8, 9)) {
+			setValidDate(false);
+			return;
+		}
+
+		if (newDate > new Date()) {
+			setValidDate(false);
+			return;
+		}
+		setCurrDate(newDate);
 	}
 
 	const submitGuess = (e: any) => {
@@ -119,6 +169,13 @@ export default function Game() {
 		<>
 			<img src={cinnamonLogo} className="logo" alt="Linkedin logo" />
 			<h1 className='cinnamon-message'>The Daily Cinnamon</h1>
+
+			<div className='date-container'>
+				<button onClick={prevDate}><strong>&lt; Prev</strong></button>
+				<input type='date' onChange={onDateChange} value={`${currDate.getFullYear()}-${(currDate.getMonth() + 1) >= 10 ? (currDate.getMonth() + 1) : "0" + (currDate.getMonth() + 1)}-${(currDate.getDate()) >= 10 ? currDate.getDate() : "0" + currDate.getDate()}`} />
+				<button onClick={nextDate}><strong>Next &gt;</strong></button>
+			</div>
+			{!validDate && <p className='error-message'>Date must be after 09/09/24 and before current date ({new Date().toLocaleDateString()})</p>}
 
 			<h2><em>{partOfSpeech}</em></h2>
 			{definitions.map((definition) => (
